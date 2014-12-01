dee715be5556dc269deb2cb35f773d971069e7b7
yingjieyao <yyjhit@sina.com>
2014-12-01 21:53:40 +0800

    thread

--- Client.java
+++ Client.java
@@ -5,25 +5,29 @@ import java.net.Socket;
 import java.io.*;
 import java.util.*;
 
+
+
 public class Client {  
     Socket client;  
     OutputStream os=null;  
     FileInputStream filein=null;
     public Client() throws UnknownHostException, IOException {  
-        client=new Socket("127.0.0.1",7777);  
+        client=new Socket("localhost",7777);  
         os=(client.getOutputStream());
         Scanner cin = new Scanner(System.in);
-        String s=cin.nextLine();
-        filein=new FileInputStream(s); 
-        os.write(s.getBytes());
-       // BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-        int data;
-        while((data=filein.read())!=-1){
-            os.write(data);
-            System.out.println(data);
-        }
-      //  os.write(br.readLine().getBytes());  
-        os.close();  
+            String s=cin.next();
+            client=new Socket("localhost",7777);  
+            os=(client.getOutputStream());
+            filein=new FileInputStream(s);
+            // s=s+"\0"; 
+            os.write(s.getBytes());
+            int data;
+            while((data=filein.read())!=-1){
+                os.write(data);
+                System.out.println(data);
+            }
+         os.close();  
+        // while(true);
     }  
     public static void main(String[] args) {  
         try {  
--- Server.java
+++ Server.java
@@ -6,28 +6,59 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-public class Server {  
+import java.util.*;
+
+
+
+public class Server implements Runnable {  
     private Socket socket;  
-    private ServerSocket ss;  
-    
+    private ServerSocket ss; 
+    private static int FileReceCnt=0;
+    public Server(Socket s){
+        super();
+        this.socket=s;
+    }
     public Server() throws IOException {  
-        ss = new ServerSocket(7777);
-        byte[] buf = new byte[1000];  
-        while (true) {  
-            socket = ss.accept();  
-            InputStream is = socket.getInputStream();
+        try{
+            ss = new ServerSocket(7777);
+            while(true){
+                 socket = ss.accept();  
+                  new Thread(new Server(socket)).start();
+                  FileReceCnt++;
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }  
+    public void run(){
+        byte[] buf=new byte[100];
+        InputStream is=null;
+        FileOutputStream fos=null;
+         try{
+            // byte[] buf = new byte[1000];  
+            is = socket.getInputStream();
+            Arrays.fill(buf,(byte)0);
             int len=is.read(buf);
-            String filename= new String(buf,0,len);
+            String filename= new String(buf);
             System.out.println(filename);
-            FileOutputStream fos=new FileOutputStream("new"+filename);
+            fos=new FileOutputStream(FileReceCnt+filename);
             int data;
             while((data=is.read())!=-1){
                 fos.write(data);
-                System.out.print((char)(data));
             }
-        }  
-    }  
-  
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(fos!=null)fos.close();
+                if(is!=null)is.close();
+                this.socket.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
     public static void main(String[] args) {  
         try {  
             new Server();  
